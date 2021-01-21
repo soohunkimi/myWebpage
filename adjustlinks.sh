072934a5b2920f21b882b4badddf434bbea5589f
Soohun Kim <sk@Soohuns-MacBook-Pro.local>
2021-01-21 16:02:41 +0900

    20200121

---
+++ adjustlinks.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sed -i '' 's|href="https|target="_blank" href="https|g' index.html
+sed -i '' 's|href="https|target="_blank" href="https|g' research.html
+sed -i '' 's|href="https|target="_blank" href="https|g' media.html
